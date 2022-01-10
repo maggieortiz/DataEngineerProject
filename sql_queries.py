e8da415f45c35b68d74103c21e2667f4aaea1fd8
Margaret Ortiz <margaret.barbero@gmail.com>
2022-01-10 18:37:20 -0500

    Add sql queries file & initial structure

---
+++ sql_queries.py
@@ -0,0 +1,20 @@
+import configparser
+
+
+# CONFIG
+config = configparser.ConfigParser()
+config.read('dwh.cfg')
+
+# DROP TABLES
+
+# CREATE TABLES
+
+# CREATE TABLES
+
+# FINAL TABLES (Insert)
+
+# QUERY LISTS
+create_tables_queries = 
+drop_tables_queries = 
+copy_table_queries = 
+insert_table_queries = 
