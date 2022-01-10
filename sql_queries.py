16319ad53c0239f0b363f16305cdec3d60ba507c
Margaret Ortiz <margaret.barbero@gmail.com>
2022-01-10 18:41:45 -0500

    Add Drop table queries

--- sql_queries.py
+++ sql_queries.py
@@ -6,6 +6,10 @@ config = configparser.ConfigParser()
 config.read('dwh.cfg')
 
 # DROP TABLES
+co2emission_table_drop = "DROP TABLE IF EXISTS co2_emission;"
+population_table_drop = "DROP TABLE IF EXISTS population;"
+surfacetemp_table_drop = "DROP TABLE IF EXISTS surface_temp;"
+naturaldisaster_table_drop = "DROP TABLE IF EXISTS natural_disaster;"
 
 # CREATE TABLES
 
