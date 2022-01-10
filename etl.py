945aa746919eb6775c636394cafdec4ef4c628cd
Margaret Ortiz <margaret.barbero@gmail.com>
2022-01-10 18:32:49 -0500

    basic data warehouse ETL

--- etl.py
+++ etl.py
@@ -1,3 +1,34 @@
 # This is where the ETL will transform the raw data into a data warehouse or data lake
 
-# test if i can still use git
+import configparser
+import psycopg2
+from sql_queries import copy_table_queries, insert_table_queries
+
+
+def load_staging_tables(cur, conn):
+    for query in copy_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def insert_tables(cur, conn):
+    for query in insert_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    cur = conn.cursor()
+    
+    load_staging_tables(cur, conn)
+    insert_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
