2df782fadf33ba3f361d9272c18baf14d69889a2
Margaret Ortiz <margaret.barbero@gmail.com>
2022-01-21 16:00:33 -0500

    fixed insert statements

--- sql_queries.py
+++ sql_queries.py
@@ -16,7 +16,7 @@ population_stage_table_drop = "DROP TABLE IF EXISTS population_stage;"
 surfacetemp_stage_table_drop = "DROP TABLE IF EXISTS surfacetemp_stage;"
 naturaldisaster_stage_table_drop = "DROP TABLE IF EXISTS naturaldisaster_stage;"
 # CREATE TABLES
-co2emission_create_table = ("""CREATE TABLE IF NOT EXISTS co2emssion(
+co2emission_create_table = ("""CREATE TABLE IF NOT EXISTS co2emission(
         country varchar NOT NULL,
         code varchar,
         year int NOT NULL, 
@@ -40,13 +40,13 @@ surfacetemp_create_table = ("""CREATE TABLE IF NOT EXISTS surfacetemp(
         """)
 naturaldisasterinfo_create_table = ("""CREATE TABLE IF NOT EXISTS naturaldisaster_info(
         year int NOT NULL, 
-        country varchar,
+        country varchar NOT NULL,
         region varchar,
         location varchar(5000),
         seq int, 
         glide varchar, 
-        gisaster_group varchar,
-        gisaster_subgroup varchar,
+        disaster_group varchar,
+        disaster_subgroup varchar,
         diaster_type varchar, 
         disaster_subtype varchar,
         disaster_subsubtype varchar,
@@ -57,7 +57,7 @@ naturaldisasterinfo_create_table = ("""CREATE TABLE IF NOT EXISTS naturaldisaste
          """)
 naturaldisasterdamage_create_table = ("""CREATE TABLE IF NOT EXISTS naturaldisaster_damage(
         year int NOT NULL, 
-        country varchar,
+        country varchar NOT NULL,
         event_name varchar,
         total_deaths FLOAT,
         no_injured FLOAT,
@@ -144,21 +144,21 @@ naturaldisaster_staging_create_table = ("""CREATE TABLE IF NOT EXISTS naturaldis
 co2emission_copy_table = ("""Copy co2emission_stage
         from {}
         iam_role {}
-        Csv NOLOAD
+        CSV
         IGNOREHEADER 1
         delimiter ',' ;""").format(config['S3']['CSV_CO2EMISSION'], config['IAM_ROLE']['ARN'] )
 
 population_copy_table = ("""Copy population_stage
         from  {}
         iam_role {} 
-        Csv NOLOAD
+        CSV
         IGNOREHEADER 1
         delimiter ',' ;""").format(config['S3']['CSV_POPULATION'], config['IAM_ROLE']['ARN'])
 
 surfacetemp_copy_table = ("""Copy surfacetemp_stage
         from {}
         iam_role {}
-        Csv NOLOAD
+        CSV
         IGNOREHEADER 1 
         delimiter ','
          ;""").format(config['S3']['CSV_SURFACETEMP'], config['IAM_ROLE']['ARN'])
@@ -166,73 +166,102 @@ surfacetemp_copy_table = ("""Copy surfacetemp_stage
 naturaldisaster_copy_table = ("""Copy naturaldisaster_stage
         from {}
         iam_role {}
-        csv NOLOAD
+        CSV
         IGNOREHEADER 1 
         delimiter ','      ; """).format(config['S3']['CSV_NATURALDISASTER'], config['IAM_ROLE']['ARN'])
 # FINAL TABLES (Insert)
 co2emission_table_insert = ("""INSERT INTO co2emission 
-    (country, code, year, co2emission BIGINT)
+    (country, code, year, co2emission)
     SELECT DISTINCT 
-        country NOT NULL, 
+        country, 
         code,
-        year NOT NULL,
-        co2emission 
+        year,
+        CO2emission as co2emission 
         FROM co2emission_stage ; 
 """)
 population_table_insert = ("""INSERT INTO population 
-    (country, year, population BIGINT)
+    (country, year, population)
     SELECT DISTINCT 
-        country NOT NULL, 
-        year NOT NULL,
+        country , 
+        year,
         population 
         FROM population_stage ; 
 """)
 
 surfacetemp_table_insert = ("""INSERT INTO surfacetemp 
-    (year, month, country, avg_temperature, avg_temperature_uncertainty )
+    (year, month, country, avg_temperature, avg_temperature_uncertainty)
     SELECT DISTINCT 
-        extract(year from dt) as year NOT NULL,
+        EXTRACT(year from dt) as year ,
         EXTRACT(month from dt) as month,
-        Country as country NOT NULL, 
+        Country as country, 
         AverageTemperature as avg_temperature,
         AverageTemperatureUncertainty as avg_temperature_uncertainty 
         FROM surfacetemp_stage ; 
 """)
 
-naturaldisasterinfo_table_insert = ("""INSERT INTO naturaldiaster_info  
-        year int NOT NULL, 
-        country varchar NOT NULL,
-        region varchar,
-        location varchar,
-        seq int, 
-        glide varchar, 
-        gisaster_group varchar,
-        gisaster_subgroup varchar,
-        diaster_type varchar, 
-        disaster_subtype varchar,
-        disaster_subsubtype varchar,
-        event_name varchar,
-        geo_locations varchar, 
-        latitude FLOAT,
-        longitude FLOAT
+naturaldisasterinfo_table_insert = ("""INSERT INTO naturaldisaster_info
+        (year, 
+        country,
+        region,
+        location,
+        seq, 
+        glide, 
+        disaster_group,
+        disaster_subgroup,
+        diaster_type, 
+        disaster_subtype,
+        disaster_subsubtype,
+        event_name,
+        geo_locations, 
+        latitude,
+        longitude)
+        SELECT DISTINCT
+        year, 
+        country,
+        region,
+        location,
+        seq, 
+        glide, 
+        disaster_group,
+        disaster_subgroup,
+        diaster_type, 
+        disaster_subtype,
+        disaster_subsubtype,
+        event_name,
+        geo_locations, 
+        latitude,
+        longitude
         FROM naturaldisaster_stage ; 
                  """)
-naturaldisasterdamage_table_insert = ("""INSERT INTO naturaldisaster_damage
-        naturaldisaster_id SERIAL PRIMARY KEY NOT NULL, 
-        year int, 
-        country varchar,
-        region varchar,
-        event_name varchar,
-        total_deaths FLOAT,
-        no_injured FLOAT,
-        no_affectED FLOAT,
-        no_homeless FLOAT,
-        total_affected FLOAT,
-        insured_damages FLOAT,
-        total_damages FLOAT,
-        cpi FLOAT,
-        aid_contribution FLOAT,
-        OFDA_response varchar
+naturaldisasterdamage_table_insert = ("""INSERT INTO naturaldisaster_damage 
+        (year, 
+        country,
+        event_name,
+        total_deaths,
+        no_injured,
+        no_affectED,
+        no_homeless,
+        total_affected,
+        insured_damages ,
+        total_damages,
+        cpi,
+        aid_contribution,
+        OFDA_response)
+
+        SELECT DISTINCT
+        year, 
+        country,
+        event_name,
+        total_deaths,
+        no_injured,
+        no_affectED,
+        no_homeless,
+        total_affected,
+        insured_damages ,
+        total_damages,
+        cpi,
+        aid_contribution,
+        OFDA_response
         FROM naturaldisaster_stage;
          """)
 # QUERY LISTS
